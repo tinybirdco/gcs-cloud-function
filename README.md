ae6199cdf2033483fabe4888a8f5947f3f7cbc2a
alrocar <alrocar@tinybird.co>
2021-03-23 17:21:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# GCS + cloud function -> Automatically ingesting to Tinybird from Google Cloud Storage
+
+This repository contains a instructions to configure a Google Cloud function that is automatically triggered each time a new CSV file is uploaded to a given Google Cloud Storage bucket.
+
+The Google Cloud function function appends the contents of the CSV file to a Tinybird Data Source.
+
+## How to deploy the Cloud Function
+
+Install the `gcloud` command line tool and run these commands:
+
+```sh
+# edit the .env.yaml file to set your Tinybird token available from https://ui.tinybird.co/tokens
+cp .env.yaml.sample .env.yaml
+
+# set some environment variables before deploying
+PROJECT_NAME=<the_GCP_project_name>
+SERVICE_ACCOUNT_NAME=<service_account_name@project_name.iam.gserviceaccount.com>
+BUCKET_NAME=<bucket_name>
+REGION=<region>
+TB_FUNCTION_NAME=<name_of_the_function>
+
+# grant permissions to deploy the cloud function and read from storage to the service account
+gcloud projects add-iam-policy-binding $PROJECT_NAME --member serviceAccount:$SERVICE_ACCOUNT_NAME --role roles/storage.admin
+gcloud projects add-iam-policy-binding $PROJECT_NAME --member serviceAccount:$SERVICE_ACCOUNT_NAME --role roles/iam.serviceAccountTokenCreator
+gcloud projects add-iam-policy-binding $PROJECT_NAME --member serviceAccount:$SERVICE_ACCOUNT_NAME --role roles/editor
+
+# deploy the cloud function
+gcloud functions deploy $TB_FUNCTION_NAME \
+--runtime python38 \
+--trigger-resource $BUCKET_NAME \
+--trigger-event google.storage.object.finalize \
+--region $REGION \
+--env-vars-file .env.yaml \
+--service-account $SERVICE_ACCOUNT_NAME
+```
+
+Once finished, go to the Google Cloud console and check the cloud function is there and the `TB_TOKEN` and `FILE_REGEXP` variables required to run the script are available.
+
+![](output.gif)
---
+++ main.py
@@ -0,0 +1,96 @@
+import re
+import os
+import google.auth
+from google.auth.transport import requests
+from google.auth import compute_engine
+from google.cloud import storage
+from datetime import datetime, timedelta
+import json
+import logging
+import urllib3
+from urllib.parse import urlencode
+
+# Tinybird datasource url
+url = f'https://api.tinybird.co/v0/datasources?'
+
+def process_name(key):
+    key = key.replace('.csv', '')
+    file_regexp = os.getenv('FILE_REGEXP', None)
+    if not file_regexp:
+        return key
+    m = re.search(file_regexp, key)
+    return m.group(0) if m is not None else key
+
+def create_presigned_url(bucket_name, object_name, expiration=3600):
+    from google.cloud import storage
+    from datetime import datetime, timedelta
+
+    import google.auth
+    credentials, project_id = google.auth.default()
+
+    # Perform a refresh request to get the access token of the current credentials (Else, it's None)
+    from google.auth.transport import requests
+    r = requests.Request()
+    credentials.refresh(r)
+
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.get_blob(object_name)
+    expires = datetime.now() + timedelta(seconds=86400)
+
+    # In case of user credential use, define manually the service account to use (for development purpose only)
+    service_account_email = "YOUR DEV SERVICE ACCOUNT"
+    # If you use a service account credential, you can use the embedded email
+    if hasattr(credentials, "service_account_email"):
+        service_account_email = credentials.service_account_email
+
+    url = blob.generate_signed_url(expiration=expires,service_account_email=service_account_email, access_token=credentials.token)
+    return url
+
+def upload_to_tinybird(csv_path, name):
+    http = urllib3.PoolManager()
+    fields = {
+        'mode': 'append',
+        'name': name.replace('.csv',''),
+        'url': csv_path
+    }
+    headers={
+        'Authorization': 'Bearer ' + os.environ['TB_TOKEN']
+    }
+    return http.request('POST', url + urlencode(fields), headers=headers)
+
+def ingest_to_tinybird(event, context):
+    """Background Cloud Function to be triggered by Cloud Storage.
+       This generic function logs relevant data when a file is changed.
+
+    Args:
+        event (dict):  The dictionary with data specific to this type of event.
+                       The `data` field contains a description of the event in
+                       the Cloud Storage `object` format described here:
+                       https://cloud.google.com/storage/docs/json_api/v1/objects#resource
+        context (google.cloud.functions.Context): Metadata of triggering event.
+    Returns:
+        None; the output is written to Stackdriver Logging
+    """
+
+    status = 200
+    responses = []
+
+    # for record in event['Records']:
+    bucket = event['bucket']
+    key = event['name']
+    csv_path = create_presigned_url(bucket, key)
+    name = process_name(key)
+    r = upload_to_tinybird(csv_path, name=name)
+    status = r.status if r.status != 200 else status
+    responses.append({
+        'status': r.status,
+        'name': name,
+        'bucket': bucket,
+        'tb_data': json.loads(r.data.decode('utf-8'))
+    })
+
+    print(json.dumps({
+        'status': status,
+        'responses': responses
+    }))
